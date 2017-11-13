48b34bffb65584ce6274c2d0e4e2565ba014f9fc
Braden Jageman <bjageman@gmail.com>
2017-11-13 13:58:06 -0800

    added some cool tables

--- src/adventure/logs/components/characters/List.js
+++ src/adventure/logs/components/characters/List.js
@@ -3,6 +3,8 @@ import React from 'react'
 import { connect } from 'react-redux'
 import { mapStateToProps, mapDispatchToProps } from 'redux/utils'
 
+import ReactTable from 'react-table'
+import 'react-table/react-table.css'
 import { Text, Button, Table, TableRow } from 'bjageman-react-toolkit'
 
 import ReduxLink from 'base/components/links/Redux'
@@ -15,19 +17,49 @@ class CharacterListing extends React.Component {
         })
     }
 
+    handleDelete = (id) => {
+        alert("Delete " + id + "?")
+    }
+
     render(){
         const characters = this.props.logs.characters.listing
+        const columns = [
+        {
+            id: 'name',
+            Header: 'Link',
+            accessor: d => d, // Custom value accessors!
+            Cell: props => <ReduxLink to={"/logs/characters/" + props.value.id}><Button style={{margin:0, padding:0}}>{props.value.name}</Button></ReduxLink>
+        },
+        {
+            Header: 'Race',
+            accessor: 'race'
+        },{
+            Header: 'Class',
+            accessor: 'class'
+        },{
+            Header: 'Background',
+            accessor: 'background'
+        },
+        // {
+        //     Header: 'Options',
+        //     accessor: 'id',
+        //     Cell: props => (
+        //         <Button onClick={() => this.handleDelete(props.value)} style={{margin:0, padding:0}} >Delete</Button>
+        //         )
+        // }
+        ]
         return(
             <div>
                 <Text h2>Character Logs</Text>
                 <ReduxLink to="/logs/characters/new">
                     <Button raised>New Character</Button>
                 </ReduxLink>
-                    <Table headers={["Name", "Race", "Class", "Background", "Level"]}>
-                        { characters.map((character, i) =>
-                            <TableRow rows={[character.name, character.race, character.class, character.background, 0]} />
-                        )}
-                    </Table>
+                <ReactTable
+                    data={characters}
+                    columns={columns}
+                    minRows={5}
+                    minWidth={0}
+                    />
             </div>
         )
     }
